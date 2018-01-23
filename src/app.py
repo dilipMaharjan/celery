b8d2fb04b74a6a3a406430a49e7870a1d3207e40
dilipMaharjan <dilipmaharjann@gmail.com>
2018-01-23 11:53:42 -0500

    display number of seconds required to complete the task

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
 from tasks import *
+import datetime 
 
+dt_start=datetime.datetime.now().second
 result=add.delay(2,3)
 if result.ready:
-    print(result.get())
+    for i in range(1,10000):
+        print(result.get())
+    print("Time taken: {} secs".format(datetime.datetime.now().second-dt_start ))
